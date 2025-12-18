fb8bd97d08609fa3794963b105953f17bd7e3a36
Mahmoud Abo-Zied <116457362+EpitomeZied@users.noreply.github.com>
2025-12-19 01:16:54 +0200

    2747-APPLY-TRANSFORM-OVER-EACH-ELEMENT-IN-ARRAY Stats: Time: 45 ms (60.85%), Space: 53.2 MB (93.96%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map(fn);
+    let a = new Array(arr.length);
+    for(let i = 0; i < arr.length; i++){
+        a[i] = fn(arr[i], i);
+    }
+    return a;
 };
