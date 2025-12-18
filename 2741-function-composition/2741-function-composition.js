be35f932f4c52ea83d8e78964355378a29214d1c
Mahmoud Abo-Zied <116457362+EpitomeZied@users.noreply.github.com>
2025-12-19 01:21:26 +0200

    2741-FUNCTION-COMPOSITION Stats: Time: 60 ms (45.34%), Space: 55 MB (95.16%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let a = x;
+        for(let i = functions.length - 1; i >= 0; i--){
+            a = functions[i](a);
+        }
+        return a;
+    }
+};
